d1582d5fa1c640e593f9e14a53c8155a8e6e2df2
Ganavi J <ganavijayaram1996@gmail.com>
2018-07-29 14:46:20 +0530

    ADD GET /users/me and authenticate

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ var {User} = require('./models/user.js');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
+var {authenticate} = require('./middleware/authenticate.js');
+
 
 var app = express();
 // for heoku
@@ -96,28 +98,32 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+
 app.listen(port, () => {
 
 	// for heroku
 	console.log(`Started on port ${port}`);
 });
 
-// POST /users
-app.post('/users', (req, res) => {
-  // console.log('body', req);
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
-  // console.log('body', body);
-  user.save().then(() => {
-  	return user.generateAuthToken();
-    // res.send(user);
-  }).then((token) => {
-  	// console.log('token', token);
-  	res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
-});
+
 
 
 
